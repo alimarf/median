c456e25db6b097c8cccb1a91b66e21390d558772
alimarief <alimarief97@gmail.com>
2024-08-24 02:53:14 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
+// import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+// import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const { httpAdapter } = app.get(HttpAdapterHost);
+  // const { httpAdapter } = app.get(HttpAdapterHost);
 
-  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  // app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   // app.enableCors({
   //   origin: 'https://median-k7k6wa7kg-alims-projects-6beec4b0.vercel.app', // Replace with your frontend domain
