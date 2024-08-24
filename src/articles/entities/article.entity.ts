f66a6c0f2fbf1c80763165bafc97c2b8ea49ad83
alimarief <alimarief97@gmail.com>
2024-08-25 00:12:15 +0700

    fix: user entity import

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,5 +1,5 @@
 import { Article } from '@prisma/client';
-import { UserEntity } from 'src/users/entities/user.entity';
+import { UserEntity } from '../../users/entities/user.entity';
 
 export class ArticleEntity implements Article {
   id: number;
